057f759c859cba192cf5b646b9c8a41e0d2bb0ee
Adam <amuslimov-07@yandex.ru>
2026-09-03 16:14:57 +0300

    Реализовал регистрацию, логин, логаут и jwt токены

---
+++ backend/models/user-model.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  isActivated: { type: Boolean, default: false },
+  activationLink: { type: String },
+  cleanedCount: { type: Number, default: 0 },
+  cleanedDistance: { type: Number, default: 0 },
+  refCount: { type: Number, default: 0 },
+  placesCount: { type: Number, default: 0 },
+});
+
+module.exports = model("User", UserSchema);
